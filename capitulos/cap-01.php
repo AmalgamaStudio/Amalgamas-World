3be1fdb7757f35b5de133eb35a7c64d0428b926f
Amalgama's Studios <163958488+AmalgamaStudio@users.noreply.github.com>
2024-05-18 17:08:08 -0700

    Add files via upload

---
+++ capitulos/cap-01.php
@@ -0,0 +1,14 @@
+<?php require('../layout/header-1.php')?>
+
+    <main>
+    <div class="text01">
+        <h1>WIP</h1>
+    	    <p>ESTO ES SOLO UNA PRUEBA</p>
+    	    <p><video width="854" height="480" controls>
+ 	    <source src="../videos/wip01.mp4" type="video/mp4">
+	    Your browser does not support the video tag.
+	    </video></p>
+    </div>
+    </main>
+
+<?php require('../layout/footer.php')?>
