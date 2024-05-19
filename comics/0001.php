6f0ef9689c5f79755422e56b335bbbc65cf6fbae
Amalgama's Studios <163958488+AmalgamaStudio@users.noreply.github.com>
2024-05-18 17:06:46 -0700

    Add files via upload

---
+++ comics/0001.php
@@ -0,0 +1,11 @@
+<?php require('../layout/header-1.php')?>
+
+    <main>
+    <div class="text01">
+        <h1>Comic 1</h1>
+    	    <p>Test atte. Mike</p>
+    	    <p><img src="../comics/img/0001.png" width="" height="" alt="Comic" /></p>
+    </div>
+    </main>
+
+<?php require('../layout/footer.php')?>
